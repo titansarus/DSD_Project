72d218dfc5bfa06999df6a61c3568deee9eb4e23
titansarus <amirm137878@gmail.com>
2020-08-03 02:23:55 +0430

    Add top module

---
+++ VECTOR/TopModule.v
@@ -0,0 +1,17 @@
+`include "CONSTANTS.v"
+
+module TopModule(input clock,
+    input signed [31:0] x,
+    input signed [31:0] y,
+    input signed [31:0] angle,
+    input [1:0] mode,
+    output wire signed [31:0] rotated_x,
+    output wire signed [31:0] rotated_y,
+    output wire signed [31:0] final_angle);
+
+
+
+CORDIC_Vector #(17) cr  (.clock(clock),.x(x) ,.y(y) , .angle(angle) , .mode(mode) , .rotated_x(rotated_x) , .rotated_y(rotated_y) , .final_angle(final_angle));
+
+
+endmodule
