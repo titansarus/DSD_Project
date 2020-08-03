7213d3eda5bafed41cf79920b01da9c1cdb652b6
Ali Ghasemi <45513867+hesanghasemi@users.noreply.github.com>
2020-08-03 11:20:31 +0430

    Add files via upload

--- ROTATION/tb_Z_Calculator.v
+++ ROTATION/tb_Z_Calculator.v
@@ -1,8 +1,8 @@
+
 `include "CONSTANTS.v"
 
 module tb_X_Calculator();
     reg [31:0] angle;
-    reg[31:0] y;
     reg [31:0] lookup_table_amount;
     reg clock;
     wire [31:0] angle_out;
@@ -13,7 +13,7 @@ module tb_X_Calculator();
     
     
     
-    Z_Calculator zcal(.angle(angle), .y(y), .lookup_table_amount(lookup_table_amount), .clock(clock), .angle_out(angle_out));
+    Z_Calculator zcal(.angle(angle), .lookup_table_amount(lookup_table_amount), .clock(clock), .angle_out(angle_out));
     
  initial 
   begin
@@ -28,16 +28,13 @@ module tb_X_Calculator();
     initial
     begin
     angle<=32'b00010000000000000000000000000000;
-    y <= 32'd40;
-    lookup_table_amount <= 32'b00001111000000000000000000000000;
+    lookup_table_amount<=32'b00100111000000011000100000100000;
     #Time_Between_Tests
     angle <= 32'b00011100000000000000000000000000;
-    y <= 32'd20;
-    lookup_table_amount <=32'b01011110000000000000000000000000;
+    lookup_table_amount<=32'b00100111001100011000101100100000;
     #Time_Between_Tests
     angle <= 32'b01010110000000000000000000000000;
-    y <= 32'd30;
-    lookup_table_amount <= 32'b00111110000000000000000000000000;  
+    lookup_table_amount<=32'b11100111001100011000101100100011;
     end
     initial
   begin
@@ -45,5 +42,4 @@ module tb_X_Calculator();
   end
 endmodule
   
-
 
